bd6be3fcef5fca7ca6701e0bde7ac30711831934
Chetan Raj <130752878+crajnc@users.noreply.github.com>
2023-04-17 16:12:37 +0000

    some beautifications

--- pages/Empowering.js
+++ pages/Empowering.js
@@ -4,20 +4,22 @@ import { Raleway } from "next/font/google";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
+import Wave from "./components/Wave";
+
 const Empowering = () => {
   return (
     <section
-      className={`mx-auto text-center px-12 py-24 bg-nc-purple ${raleway.className}`}
+      className={`mx-auto text-center px-12 py-24 bg-nc-gradient relative ${raleway.className}`}
     >
-      <h1 className="w-full my-2 text-5xl font-bold text-center text-slate-50">
-        World&lsquo; s first company empowering self waste management.
+      <h1 className="w-full my-2 text-3xl font-bold text-center">
+        World&lsquo;s first company empowering self waste management.
       </h1>
-      <div className="my-4 text-slate-50">
+      <div className="my-4">
         Enabling individuals to take control of their own waste. Whether
         you&lsquo;re a GenZ, Millenial, or Baby Boomer, waste is something that
-        we all generate and it&lsquo;s high time that we take charge of it so
-        that our future generations aren&lsquo;t born into a world drowning in
-        waste.
+        we all generate and <br /> it&lsquo;s high time that we take charge of
+        it so that our future generations aren&lsquo;t born into a world
+        drowning in waste.
       </div>
     </section>
   );
