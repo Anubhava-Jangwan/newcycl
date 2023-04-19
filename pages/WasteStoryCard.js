40b3b17706423b4d7c743f8ec2b74dab757bd346
Chetan Raj <130752878+crajnc@users.noreply.github.com>
2023-04-19 10:26:48 +0000

    undefined check

--- pages/WasteStoryCard.js
+++ pages/WasteStoryCard.js
@@ -31,7 +31,7 @@ export default function WasteStoryCard({ story, index }) {
       className={`flex flex-col shrink-0 p-6 bg-slate-50 mr-6 rounded-md w-80 md:w-96 lg:w-96 xl:w-96 2xl:w-96 min-h-60 ${openSans.className}`}
     >
       <div className="flex shrink-0">
-        <div className="text-lg mr-4">{story.feedback}</div>
+        <div className="text-lg mr-4">{story && story.feedback}</div>
         <button
           onClick={upVote}
           className="upvote text-center w-36 h-8 p-1 rounded-md border border-gray-500 bg-transparent"
@@ -44,8 +44,8 @@ export default function WasteStoryCard({ story, index }) {
           <div className="w-8 h-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 bg-nc-purple"></div>
         </div>
         <div className="ml-2">
-          <p className="font-medium">{story.name}</p>
-          <p className="text-size-small">{story.location}</p>
+          <p className="font-medium">{story && story.name}</p>
+          <p className="text-size-small">{story && story.location}</p>
         </div>
       </div>
     </div>
