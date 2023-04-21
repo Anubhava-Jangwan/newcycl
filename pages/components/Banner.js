76ce0739b0a8e59624aa49a3e6635f4c5faa22bb
Chetan Raj <130752878+crajnc@users.noreply.github.com>
2023-04-21 21:38:15 +0530

    Update Banner.js

--- pages/components/Banner.js
+++ pages/components/Banner.js
@@ -3,19 +3,19 @@ import { X } from "react-feather";
 
 export default function Banner() {
   return (
-    <div class="z-50 flex justify-between w-full p-4 bg-nc-purple">
-      <div class="flex items-center mx-auto">
-        <p class="flex items-center text-md text-slate-50">
+    <div className="z-50 flex justify-between w-full px-4 py-2 bg-nc-purple">
+      <div className="flex items-center mx-auto">
+        <p className="flex items-center text-md text-slate-50">
           <span>Invest in Our Planet - The theme for this Earth Month 🌏</span>
         </p>
       </div>
-      <div class="flex items-center">
+      <div className="flex items-center">
         <button
           type="button"
-          class="flex-shrink-0 inline-flex justify-center items-center text-slate-50 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white"
+          className="flex-shrink-0 inline-flex justify-center items-center text-slate-50 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white"
         >
           <X />
-          <span class="sr-only">Close banner</span>
+          <span className="sr-only">Close banner</span>
         </button>
       </div>
     </div>
