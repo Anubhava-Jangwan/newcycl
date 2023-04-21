7853f9d1dd44085c9922f73412fb7234b6770281
Chetan Raj <130752878+crajnc@users.noreply.github.com>
2023-04-21 22:20:51 +0530

    Update Empowering.js

--- pages/Empowering.js
+++ pages/Empowering.js
@@ -1,22 +1,20 @@
 import React from "react";
 
-import Wave from "./components/Wave";
-
 const Empowering = () => {
   return (
-    <section className={`mx-auto text-center px-12 py-48 relative`}>
-      <h1 className="w-full my-2 text-5xl font-bold text-center">
-        World&lsquo;s first company empowering self waste management.
+    <section className={`mx-auto container px-12 py-48 relative`}>
+      <h1 className="w-full my-2 text-5xl font-bold text-center waste-nothing relative">
+        World&rsquo;s first company empowering self waste management.
       </h1>
-      <div className="my-4">
+      <div className="my-4 text-center">
         Enabling individuals to take control of their own waste. Whether
-        you&lsquo;re a GenZ, Millenial, or Baby Boomer, waste is something that
-        we all generate and <br /> it&lsquo;s high time that we take charge of
-        it so that our future generations aren&lsquo;t born into a world
+        you&rsquo;re a GenZ, Millenial, or Baby Boomer, waste is something that
+        we all generate and <br /> it&rsquo;s high time that we take charge of
+        it so that our future generations aren&rsquo;t born into a world
         drowning in waste.
       </div>
       <h4 className="text-3xl mt-48 text-nc-purple">
-        Here&lsquo;s what does it mean to reduce one&lsquo;s own waste,
+        Here&rsquo;s what does it mean to reduce one&rsquo;s own waste,
       </h4>
       <h4 className="text-3xl mt-2 text-nc-purple">
         in terms of CO2 emissions.
