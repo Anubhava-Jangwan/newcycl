c4efd5aa47f85318c79a60243ad56ae599ba9eb2
Chetan Raj <130752878+crajnc@users.noreply.github.com>
2023-04-21 21:47:06 +0530

    Added Wave for We Hear You

--- pages/WeHearYou.js
+++ pages/WeHearYou.js
@@ -1,14 +1,19 @@
 import React from "react";
 
+import Wave from "./components/Wave";
+
 const WeHearYou = () => {
   return (
-    <div className={`mx-auto text-center px-12 py-48 dark-bg`}>
+    <div className={`mx-auto text-center px-12 py-48 dark-bg relative`}>
       <h1 className="w-full my-2 text-5xl font-bold text-center text-slate-50">
         We hear you
       </h1>
       <div className="my-4 text-slate-50">
         What if we told you that we are working on a solution that eliminates
         the need to throw your stinky garbage altogether?
+      </div>
+      <div className="absolute -top-24 -left-96 -rotate-180">
+        <Wave />
       </div>
     </div>
   );
