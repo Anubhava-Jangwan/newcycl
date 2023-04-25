09d2a29c6f91bc42d83d2fef72370ccbe4a9bd7e
Chetan Raj <130752878+crajnc@users.noreply.github.com>
2023-04-25 17:18:13 +0000

    About Page Art

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,10 @@ import Navbar from "./Navbar";
 import Curve from "./Curve";
 import Footer from "./Footer";
 
+import Image from "next/image";
+
+import art from "../public/art.png";
+
 const Index = ({ user }) => {
   return (
     <>
@@ -18,64 +22,73 @@ const Index = ({ user }) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Navbar />
-      <section className="hero-gradient pt-36">
+      <section className="mt-36">
         <div className="relative px-12 lg:px-48 mx-auto">
-          <h1 className="text-5xl leading-none">
-            We&rsquo;re on a mission to make zero waste a{" "}
-            <span className="text-nc-purple">new normal</span>
-          </h1>
+          <div className="flex flex-col-reverse sm:flex-row justify-center items-center">
+            <h1 className="text-5xl leading-none sm:mr-12">
+              We&rsquo;re on a mission to make zero waste a{" "}
+              <span className="text-nc-purple">new normal</span>
+            </h1>{" "}
+            <div className="sm:w-10/12 mt-12">
+              <Image src={art} width={"100%"} alt="Art" className="w-full" />
+            </div>
+          </div>
 
           <div className="mt-12 text-lg leading-relaxed">
             Hey there, we are a small team of engineers and data scientists who
-            are super pumped to reduce household waste through decentralized
-            solutions! Honestly, we&rsquo;re pretty obsessed with it. We started
-            Newcycl when we realized just how much waste we produce and how
-            it&rsquo;s wrecking our environment. It&rsquo;s crazy to think that
-            all that trash is impacting our soil, air, water, and ultimately us.
+            are super pumped to reduce household waste through{" "}
+            <span className="text-nc-purple font-medium">
+              decentralized solutions!
+            </span>{" "}
+            In this age of the Internet, with{" "}
+            <span className="text-nc-purple font-medium">
+              hectic lifestyles and instant gratification
+            </span>{" "}
+            being the norm, most of our old traditions that were designed to
+            keep{" "}
+            <span className="text-nc-purple font-medium">
+              harmony with nature
+            </span>
+            , cannot be practiced in its old form. Notwithstanding this
+            unpleasant reality of urban life, we have decided to{" "}
+            <span className="text-nc-purple font-medium">
+              unravel the old valuable principles
+            </span>{" "}
+            in a modern age form.
           </div>
 
-          <h3 className="mt-12 text-2xl">The grave waste problem</h3>
+          <h3 className="mt-12 text-2xl">Our Approach & Vision</h3>
           <div className="mt-2 text-lg leading-relaxed">
-            Most garbage collection systems in use today are manual and perform
-            pick-ups that are either unnecessary or tardy. Unnecessary pickups
-            increase{" "}
-            <a
-              className="text-blue-400 underline"
-              target="_blank"
-              href="https://wastehero.io/newsroom/6-major-benefits-of-iot-waste-management-for-smart-cities/"
-            >
-              annual collection expenses by 70%
-            </a>
-            . Moreover, frequent route congestions, which often results from
-            ineffective route planning, also raise the volume of fuel consumed
-            to carry out the collections. The whole process contributes to an{" "}
-            <a
-              className="text-blue-400 underline"
-              target="_blank"
-              href="https://wastehero.io/newsroom/6-major-benefits-of-iot-waste-management-for-smart-cities/"
-            >
-              increase in carbon footprint by 50%
-            </a>
-            .<h3 className="mt-12 text-2xl">Our Vision</h3>
-            <div className="mt-2 text-lg leading-relaxed">
-              But that&rsquo;s where we come in. Our company is harnessing the
-              power of technology to create some seriously cool solutions for
-              waste reduction and management that eliminates the need of garbage
-              collection for micro waste generators i.e. households. We&rsquo;re
-              making the{" "}
-              <span className="text-nc-purple font-medium">
-                RRR - Reduce, Reuse, Recycle
-              </span>{" "}
-              easier for everyone and relentlessly finding new ways to repurpose
-              and reuse waste. It&rsquo;s all about making sustainable living
-              accessible to everyone and make waste reduction a part of our
-              lifestyle. We truly believe that the key to success lies in
-              empowering individuals to take control of their own waste and
-              together, we can make a real difference. It&rsquo;s not just about
-              making the world a better place, it&rsquo;s about creating a
-              better future for all of us. And that&rsquo;s something worth
-              getting excited about!
-            </div>
+            Our company is harnessing the power of{" "}
+            <span className="text-nc-purple font-medium">
+              AI, Technology, and Science
+            </span>
+            to create some seriously cool solutions for waste reduction and
+            management that eliminates the need of garbage collection for micro
+            waste generators i.e. households. We're making the{" "}
+            <span className="text-nc-purple font-medium">
+              RRR - Reduce, Reuse, Recycle easier for everyone
+            </span>{" "}
+            and relentlessly finding new ways to repurpose and reuse waste. It's
+            all about making sustainable living accessible to everyone and
+            making{" "}
+            <span className="text-nc-purple font-medium">
+              waste reduction a part of our lifestyle.
+            </span>
+          </div>
+
+          <h3 className="mt-12 text-2xl">Our Approach & Vision</h3>
+          <div className="mt-2 text-lg leading-relaxed">
+            We truly believe that the key to success lies in{" "}
+            <span className="text-nc-purple font-medium">
+              empowering individuals
+            </span>{" "}
+            to take control of their own waste and together, we can
+            <span className="text-nc-purple font-medium">
+              make a real difference,
+            </span>{" "}
+            creating a better future for all of us. And that's something worth
+            getting excited about!
           </div>
         </div>
       </section>
