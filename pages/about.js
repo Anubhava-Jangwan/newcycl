81a05099dd4fbc42e713bc0e7fdf3ec8fd3bc9ff
Chetan Raj <130752878+crajnc@users.noreply.github.com>
2023-04-25 17:33:33 +0000

    Changes

--- pages/about.js
+++ pages/about.js
@@ -22,76 +22,72 @@ const Index = ({ user }) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Navbar />
-      <section className="pt-24">
-        <div className="relative lg:px-48 mx-auto">
-          <div className="flex flex-col-reverse sm:flex-row justify-center items-center">
-            <h1 className="px-12 sm:px-0 text-5xl leading-none sm:mr-12">
-              We&rsquo;re on a mission to make zero waste a{" "}
-              <span className="text-nc-gradient">new normal</span>
-            </h1>{" "}
-            <div className="sm:w-10/12 mt-12">
-              <Image src={art} width={"100%"} alt="Art" className="w-full" />
-            </div>
-          </div>
-
-          <div className="px-12 mt-12 text-lg leading-relaxed">
-            Hey there, we are a small team of engineers and data scientists who
-            are super pumped to reduce household waste through{" "}
-            <span className="text-nc-purple font-medium">
-              decentralized solutions!
-            </span>{" "}
-            In this age of the Internet, with{" "}
-            <span className="text-nc-purple font-medium">
-              hectic lifestyles and instant gratification
-            </span>{" "}
-            being the norm, most of our old traditions that were designed to
-            keep{" "}
-            <span className="text-nc-purple font-medium">
-              harmony with nature
-            </span>
-            , cannot be practiced in its old form. Notwithstanding this
-            unpleasant reality of urban life, we have decided to{" "}
-            <span className="text-nc-purple font-medium">
-              unravel the old valuable principles
-            </span>{" "}
-            in a modern age form.
-          </div>
-
-          <h3 className="px-12 mt-12 text-2xl">Our Approach & Vision</h3>
-          <div className="px-12 mt-2 text-lg leading-relaxed">
-            Our company is harnessing the power of{" "}
-            <span className="text-nc-purple font-medium">
-              AI, Technology, and Science
-            </span>
-            to create some seriously cool solutions for waste reduction and
-            management that eliminates the need of garbage collection for micro
-            waste generators i.e. households. We're making the{" "}
-            <span className="text-nc-purple font-medium">
-              RRR - Reduce, Reuse, Recycle easier for everyone
-            </span>{" "}
-            and relentlessly finding new ways to repurpose and reuse waste. It's
-            all about making sustainable living accessible to everyone and
-            making{" "}
-            <span className="text-nc-purple font-medium">
-              waste reduction a part of our lifestyle.
-            </span>
-          </div>
-
-          <h3 className="px-12 mt-12 text-2xl">What we Believe</h3>
-          <div className="px-12 mt-2 text-lg leading-relaxed">
-            We truly believe that the key to success lies in{" "}
-            <span className="text-nc-purple font-medium">
-              empowering individuals
-            </span>{" "}
-            to take control of their own waste and together, we can
-            <span className="text-nc-purple font-medium">
-              make a real difference,
-            </span>{" "}
-            creating a better future for all of us. And that's something worth
-            getting excited about!
+      <div className="relative lg:px-48 mx-auto">
+        <div className="flex flex-col-reverse sm:flex-row justify-center items-center">
+          <h1 className="px-12 sm:px-0 text-5xl leading-none sm:mr-12">
+            We&rsquo;re on a mission to make zero waste a{" "}
+            <span className="text-nc-gradient">new normal</span>
+          </h1>{" "}
+          <div className="sm:w-10/12 mt-12">
+            <Image src={art} width={"100%"} alt="Art" className="w-full" />
           </div>
         </div>
-      </section>
+
+        <div className="px-12 mt-12 text-lg leading-relaxed">
+          Hey there, we are a small team of engineers and data scientists who
+          are super pumped to reduce household waste through{" "}
+          <span className="text-nc-purple font-medium">
+            decentralized solutions!
+          </span>{" "}
+          In this age of the Internet, with{" "}
+          <span className="text-nc-purple font-medium">
+            hectic lifestyles and instant gratification
+          </span>{" "}
+          being the norm, most of our old traditions that were designed to keep{" "}
+          <span className="text-nc-purple font-medium">
+            harmony with nature
+          </span>
+          , cannot be practiced in its old form. Notwithstanding this unpleasant
+          reality of urban life, we have decided to{" "}
+          <span className="text-nc-purple font-medium">
+            unravel the old valuable principles
+          </span>{" "}
+          in a modern age form.
+        </div>
+
+        <h3 className="px-12 mt-12 text-2xl">Our Approach & Vision</h3>
+        <div className="px-12 mt-2 text-lg leading-relaxed">
+          Our company is harnessing the power of{" "}
+          <span className="text-nc-purple font-medium">
+            AI, Technology, and Science
+          </span>
+          to create some seriously cool solutions for waste reduction and
+          management that eliminates the need of garbage collection for micro
+          waste generators i.e. households. We're making the{" "}
+          <span className="text-nc-purple font-medium">
+            RRR - Reduce, Reuse, Recycle easier for everyone
+          </span>{" "}
+          and relentlessly finding new ways to repurpose and reuse waste. It's
+          all about making sustainable living accessible to everyone and making{" "}
+          <span className="text-nc-purple font-medium">
+            waste reduction a part of our lifestyle.
+          </span>
+        </div>
+
+        <h3 className="px-12 mt-12 text-2xl">What we Believe</h3>
+        <div className="px-12 mt-2 text-lg leading-relaxed">
+          We truly believe that the key to success lies in{" "}
+          <span className="text-nc-purple font-medium">
+            empowering individuals
+          </span>{" "}
+          to take control of their own waste and together, we can
+          <span className="text-nc-purple font-medium">
+            make a real difference,
+          </span>{" "}
+          creating a better future for all of us. And that's something worth
+          getting excited about!
+        </div>
+      </div>
       <Curve />
       <Footer />
     </>
