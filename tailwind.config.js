1711f0a33baaf6019cc0fae141cf0f5c6b287a6d
Chetan Raj <130752878+crajnc@users.noreply.github.com>
2023-04-27 18:57:06 +0000

    tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        "nc-purple": "#5433ff",
+        "nc-purple": { 400: "#5433ff66", 900: "#5433ffcf" },
       },
       spacing: {
         112: "28rem",
