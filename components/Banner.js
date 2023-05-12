4e3772f4a245a2007366b4b954b0fb65fb70a8ee
Chetan Raj <130752878+crajnc@users.noreply.github.com>
2023-05-12 18:19:31 +0000

    Banner Changes

--- components/Banner.js
+++ components/Banner.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { X } from "react-feather";
 
 const Banner = () => {
+  const [revealAnswer, setRevealAnswer] = useState(false);
   const [hideBanner, setHideBanner] = useState(false);
 
   return (
@@ -13,19 +14,23 @@ const Banner = () => {
           hideBanner ? "hidden" : ""
         }`}
       >
-        <div className="flex items-center mx-auto">
-          <p className="flex items-center text-md">
-            <span>
-              The rate of waste production in Israel is among the highest in the
-              <Link
-                className="ml-1 underline"
-                target="_blank"
-                href="https://www.haaretz.com/opinion/2023-04-30/ty-article-opinion/.premium/israel-is-crowded-and-drowning-in-garbage-and-the-future-isnt-much-brighter/00000187-cec2-d6a1-ad87-eed3d5b50000"
+        <div className="mx-auto">
+          <div className="text-md">
+            What was the original name of the international environmental
+            organisation Greenpeace Foundation?
+          </div>
+          <div className="mt-2">
+            {revealAnswer ? (
+              <div className="italic">Don't Make a Wave Committee.</div>
+            ) : (
+              <div
+                onClick={() => setRevealAnswer(!revealAnswer)}
+                className="underline cursor-pointer"
               >
-                OECD
-              </Link>
-            </span>
-          </p>
+                Reveal Answer
+              </div>
+            )}
+          </div>
         </div>
         <div className="flex items-center">
           <button
