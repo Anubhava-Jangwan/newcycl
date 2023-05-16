5399954822ddd7fdd2b8b895dec30e73120f5e8e
Anu Khandelwal <akhandelwal@newcycl.com>
2023-05-16 15:09:09 +0530

    testing again

--- components/Earth.js
+++ components/Earth.js
@@ -18,7 +18,6 @@ const CarIcon = () => {
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
-      
       strokeLinejoin="round"
     >
       <path d="M14 16H9m10 0h3v-3.15a1 1 0 0 0-.84-.99L16 11l-2.7-3.6a1 1 0 0 0-.8-.4H5.24a2 2 0 0 0-1.8 1.1l-.8 1.63A6 6 0 0 0 2 12.42V16h2"></path>
